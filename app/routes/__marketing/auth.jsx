a54ae32e4afb0c4186baf79b60ec86fabd2a9b6f
Tom Irimescu <tom@irimescu.com>
2023-02-08 15:57:51 -0500

    Visual feedback auth form, document, catch|error boundary

--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -5,6 +5,23 @@ export default function AuthPage() {
   return <AuthForm />;
 }
 
+export async function action({ request }) {
+  //* Example of extracting search parameters in back end code
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get('mode') || 'login';
+
+  const formData = await request.formData();
+  credentials = Object.fromEntries(formData);
+
+  // validate user input
+
+  if (authMode === 'login') {
+    // login logic
+  } else {
+    // signup logic (create user)
+  }
+}
+
 export function links() {
   return [{ rel: 'stylesheet', href: authStyles }];
 }
