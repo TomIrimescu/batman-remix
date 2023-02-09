53fb3a391597fec65e5e49c95c9c98297f0f694d
Tom Irimescu <tom@irimescu.com>
2023-02-09 16:57:58 -0500

    Add user schema, update validation server & auth form

--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -1,4 +1,5 @@
 import AuthForm from '~/components/auth/AuthForm';
+import { validateCredentials } from '~/data/validation.server';
 import authStyles from '~/styles/auth.css';
 
 export default function AuthPage() {
@@ -13,7 +14,11 @@ export async function action({ request }) {
   const formData = await request.formData();
   const credentials = Object.fromEntries(formData);
 
-  // validate user input
+  try {
+    validateCredentials(credentials);
+  } catch (error) {
+    return error;
+  }
 
   if (authMode === 'login') {
     // login logic
